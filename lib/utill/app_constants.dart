1f86135fee3cc833802b529367a682ff3c44f45a
ahbadr257 <ahbadr257@gmail.com>
2022-08-01 05:07:25 +0200

    final commit

--- lib/utill/app_constants.dart
+++ lib/utill/app_constants.dart
@@ -3,7 +3,7 @@ import 'package:flutter_grocery/data/model/response/language_model.dart';
 class AppConstants {
   static const String APP_NAME = 'ZamZam';
 
-  static const String BASE_URL = 'https://rozalia.store/app';
+  static const String BASE_URL = 'https://zamzamapp.store/app';
   static const String CONFIG_URI = '/api/v1/config';
   static const String BANNER_URI = '/api/v1/banners';
   static const String CATEGORY_URI = '/api/v1/categories';
